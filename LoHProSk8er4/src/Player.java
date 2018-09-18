09341beb810ac5705e89cbecbf17264a80fc5740
saudric <lord.kaden7@gmail.com>
2018-09-17 21:57:13 -0700

    created integers for stats and such, descriptions noted -kb

--- LoHProSk8er4/src/Player.java
+++ LoHProSk8er4/src/Player.java
@@ -1,17 +1,42 @@
-import java.util.Scanner;
+
 public class Player {
+
+	//player's name -kb
+	private String name; 
+	//player's level -kb
+	private int level;
+	//current and max xp -kb
+	private int xp, maxXp
+	//current and max health -kb
+	private int health, maxHealth; 
+	//current and max mana -kb
+	private int mana, maxMana;
+	//vitality stat, will effect maxHealth -kb
+	private int vitality;
+	//intelligence stat, will effect maxMana -kb
+	private int intelligence;
+	//strength stat, will effect melee damage (multiplier) -kb
+	private int strength;
+	//wisdom stat, will effect melee damage (multiplier) -kb
+	private int wisdom; 
 	
-	public static void main(String args[]) throws Exception{ 
-	Scanner key_in = new Scanner (System.in);
-	
-	int health, maxHealth; //current and max health -kb
-	int mana, maxMana; //current and max mana -kb
-	
-	
-	
-	
-	
-	
-	
+	Player(){ //dummy character -kb
+		name = "Chicken";
+		level = 1;
+		health = 100;
+		maxHealth =	100;	
+		mana = 50;
+		maxMana = 50;
+		
+		vitality = 5;
+		intelligence = 5;
+		strength = 5;
+		wisdom = 5;
+				
+		
 	}
+	
+	
+	
+	
 }
