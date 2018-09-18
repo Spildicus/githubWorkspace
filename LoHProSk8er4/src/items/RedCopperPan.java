252165504a42aabf209ce05c4987fbdfe92aedaf
saudric <lord.kaden7@gmail.com>
2018-09-18 09:20:25 -0700

    i would like equipment formated like this please- kb

--- LoHProSk8er4/src/items/RedCopperPan.java
+++ LoHProSk8er4/src/items/RedCopperPan.java
@@ -3,7 +3,7 @@ package Items;
 public class RedCopperPan extends Equipment{
 	RedCopperPan()
 	{
-		super("Red Copper Pan", 19, "Hey there mothafuckas it's ya girl cathy, I got some brand new shit",
-				"Ultra Rare", "Weapon", 0, 250);
+		super("Red Copper Pan", 19, "Hey there mothafuckas it's ya girl cathy, I got some brand new shit", "Ultra Rare",
+	    "Weapon", 0, 250);
 	}
 }
