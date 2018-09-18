da09276fe905ce346a6ef779e3280db5edc00a4b
saudric <lord.kaden7@gmail.com>
2018-09-17 22:26:46 -0700

    created enemy, and item class -kb

---
+++ LoHProSk8er4/src/Enemy.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public abstract class Enemy {
+	
+	//enemy's name -kb
+	private String name;
+	//enemy's level -kb
+	private int level;
+	//enemy's current and max health -kb
+	private int health, maxHealth;
+	//enemy's damage -kb
+	private int damage;
+	//items enemies can drop -kb
+	private ArrayList<Item> dropList;
+
+
+}
