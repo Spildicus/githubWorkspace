850d872862cc0b5a542e7271418396956734a3ec
saudric <lord.kaden7@gmail.com>
2018-09-18 09:19:10 -0700

    fixed your mistake in capitalization of the package -kb

--- LoHProSk8er4/src/items/RedCopperPan.java
+++ LoHProSk8er4/src/items/RedCopperPan.java
@@ -1,8 +1,9 @@
-package items;
+package Items;
 
 public class RedCopperPan extends Equipment{
 	RedCopperPan()
 	{
-		super("Red Copper Pan", 19, "Hey there mothafuckas it's ya girl cathy, I got some brand new shit", "Ultra Rare", "Weapon", 0, 250);
+		super("Red Copper Pan", 19, "Hey there mothafuckas it's ya girl cathy, I got some brand new shit",
+				"Ultra Rare", "Weapon", 0, 250);
 	}
 }
