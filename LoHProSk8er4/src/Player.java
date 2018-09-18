ba27d9f8b497c99c6d9ec3eac2296c00da187f94
saudric <lord.kaden7@gmail.com>
2018-09-17 21:30:48 -0700

    created player class
    i apologize in advance

--- LoHProSk8er4/src/Player.java
+++ LoHProSk8er4/src/Player.java
@@ -1,4 +1,17 @@
-
+import java.util.Scanner;
 public class Player {
-
+	
+	public static void main(String args[]) throws Exception{ 
+	Scanner key_in = new Scanner (System.in);
+	
+	int health, maxHealth; //current and max health -kb
+	int mana, maxMana; //current and max mana -kb
+	
+	
+	
+	
+	
+	
+	
+	}
 }
